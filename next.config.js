36ea8e3c82006b56c80639f3c264ed81ad7d4df6
Kiran Babu <itskiranbabu.ai@gmail.com>
2026-01-02 15:04:53 +0530

    🔧 Fix next.config.js - Add trailingSlash for static export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: '.next',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
